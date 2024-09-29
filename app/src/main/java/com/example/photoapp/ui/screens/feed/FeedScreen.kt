929addbc2607d0a0f9842194adb1fa48fa25788b
hazaleroglu <erogluhazal@outlook.com>
2024-09-29 21:49:59 +0300

    photo adding logics added

--- app/src/main/java/com/example/photoapp/ui/screens/feed/FeedScreen.kt
+++ app/src/main/java/com/example/photoapp/ui/screens/feed/FeedScreen.kt
@@ -5,23 +5,33 @@ import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -31,13 +41,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.photoapp.data.model.Photo
 import com.example.photoapp.util.decodeBase64ToImage
@@ -61,29 +75,35 @@ fun FeedScreenView(
 ) {
     val context = LocalContext.current
     val contentResolver = context.contentResolver
-    var selectedImage by remember {
-        mutableStateOf<Uri?>(null)
-    }
+    var selectedImage by remember { mutableStateOf<Uri?>(null) }
 
     val singlePhotoPickerLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.PickVisualMedia(),
-        onResult = { uri -> selectedImage = uri }
+        onResult = { uri ->
+            if (uri != null) {
+                selectedImage = uri
+
+                loadImageToStorage.invoke(
+                    Photo(
+                        name = selectedImage?.getImageName(contentResolver),
+                        base64String = selectedImage?.toBitmap(contentResolver)
+                            ?.encodeImageToBase64() ?: ""
+                    ),
+                    {
+                        Toast.makeText(context, AppText.upload_successful, Toast.LENGTH_SHORT)
+                            .show()
+                    }, {
+                        Toast.makeText(context, AppText.upload_unsuccessful, Toast.LENGTH_SHORT)
+                            .show()
+                    })
+            }
+        }
     )
 
     fun launchPhotoPicker() {
         singlePhotoPickerLauncher.launch(
             PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
         )
-        loadImageToStorage.invoke(
-            Photo(
-                name = selectedImage?.getImageName(contentResolver),
-                base64String = selectedImage?.toBitmap(contentResolver)?.encodeImageToBase64() ?: ""
-            ),
-            {
-                Toast.makeText(context, AppText.upload_successful, Toast.LENGTH_SHORT).show()
-            }, {
-                Toast.makeText(context, AppText.upload_unsuccessful, Toast.LENGTH_SHORT).show()
-            })
     }
 
     Scaffold(
@@ -94,7 +114,10 @@ fun FeedScreenView(
         },
         floatingActionButton = {
             LargeFloatingActionButton(
-                onClick = { launchPhotoPicker() },
+                onClick = {
+                    selectedImage = null
+                    launchPhotoPicker()
+                },
                 containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
                 elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation(),
             ) {
@@ -126,18 +149,65 @@ fun PhotoGrid(photos: List<Photo>, modifier: Modifier = Modifier) {
 fun PhotoItem(photo: Photo) {
     val base64String = photo.base64String
     val bitmap = remember { base64String.decodeBase64ToImage() }
+
     bitmap?.let {
+        val image: ImageBitmap = it.asImageBitmap()
+        var isFullScreen by remember { mutableStateOf(false) }
+
         Image(
-            bitmap = it.asImageBitmap(),
+            bitmap = image,
             contentDescription = null,
             modifier = Modifier
                 .padding(4.dp)
                 .aspectRatio(1f)
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .clickable {
+                    isFullScreen = true
+                },
             contentScale = ContentScale.Crop
         )
+
+        if (isFullScreen) {
+            FullScreenImageDialog(image = image, onDismiss = { isFullScreen = false })
+        }
     }
 }
+
+
+@Composable
+fun FullScreenImageDialog(image: ImageBitmap, onDismiss: () -> Unit) {
+    Dialog(onDismissRequest = { onDismiss() }) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Image(
+                bitmap = image,
+                contentDescription = "Image",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Fit
+            )
+
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopEnd) {
+                IconButton(
+                    onClick = { onDismiss() },
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .size(48.dp)
+                        .align(Alignment.TopEnd)
+                ) {
+                    Icon(
+                        Icons.Filled.Close,
+                        contentDescription = "Close",
+                        tint = Color.White
+                    )
+                }
+            }
+        }
+    }
+}
+
 
 @Preview
 @Composable
